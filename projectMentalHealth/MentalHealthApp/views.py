6e95c0a014aeff6097415490f619af8fa163a426
mrmauler <mailsfordevraj@gmail.com>
2025-05-17 04:26:41 +0530

    add a basic webapp

--- projectMentalHealth/MentalHealthApp/views.py
+++ projectMentalHealth/MentalHealthApp/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from joblib import load
+import numpy as np
+from . import *
 
 # Create your views here.
+
+
+def predictor(request):
+    if request.method == "POST":
+
+        # Extract form data
+        depression_score = float(request.POST.get("dep_s"))
+        anxiety_score = float(request.POST.get("anx_s"))
+        productivity_score = float(request.POST.get("pro_s"))
+
+        # Load model and predict
+        model = load(
+            "D:\\XCode\\Work\\python\\Projects\\MentalHealth\\model\\model.joblib"
+        )
+
+        features = np.array(
+            [[depression_score, anxiety_score, productivity_score]]
+        )
+        pred = model.predict(features)[0]
+
+        print("----------------------------------------------------Prediction:", (pred))
+        context = {
+            "depression_score": depression_score,
+            "anxiety_score": anxiety_score,
+            "productivity_score": productivity_score,
+            "risk_score": pred.astype(int),
+
+        }
+
+        return render(request, "result.html", {"result": context})
+    return render(request, "index.html")
